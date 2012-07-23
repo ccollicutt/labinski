4fe80d57c783f741f623abc5b114bc07238785ba
admin <admin@hackavcl.ru>
2012-07-23 08:40:32 -0400

    not sure...

--- settings.py
+++ settings.py
@@ -1,5 +1,6 @@
 # Plain cirros-uec
-IMAGE = "4a7a86df-b5dc-41d0-970b-f4114778d3b2"
+#IMAGE = "4a7a86df-b5dc-41d0-970b-f4114778d3b2"
+IMAGE = "3c9db6d7-1d11-471e-aa1c-2806ccb088cd"
 #IMAGE = "c21fe6e8-9ab6-465e-87ca-ca7612cb99f9"
 #IMAGE = "d3439527-dcf9-446c-93a9-4293d2973120"
 FLAVOR = "1"
