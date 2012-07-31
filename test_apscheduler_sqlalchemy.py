67a385edefa25ba40bc5ec8682841d6bdcf6b823
vagrant <vagrant@localhost.localdomain>
2012-07-31 08:53:08 -0400

    few more parts to test apscheduler sqlalchemy

--- test_apscheduler_sqlalchemy.py
+++ test_apscheduler_sqlalchemy.py
@@ -32,11 +32,15 @@ if __name__ == '__main__':
 
 	job_name = "print_reservation_id_" + str(reservation_id)
 
+	print "====> adding start_time job"
+
 	start_instance_job = sched.add_date_job(print_reservation_id, start_time, \
 		name=job_name + '_start', args=[reservation_id])
 
+	print "====> adding later job"
+
 	start_instance_job = sched.add_date_job(print_reservation_id, later, \
-		name=job_name + '_start', args=[reservation_id])
+		name=job_name + '_later', args=[reservation_id])
 
 	print "====> Reservation id should print in 3 seconds, sleeping..."
 
